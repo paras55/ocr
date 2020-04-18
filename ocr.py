7d5b1500f2b176192e37faffa1732f40cce9271c
paras55 <52483274+paras55@users.noreply.github.com>
2020-04-19 00:23:01 +0530

    Update ocr.py

--- ocr.py
+++ ocr.py
@@ -5,4 +5,25 @@ Created on Sun Apr 19 00:13:37 2020
 @author: dell
 """
 
-ok
+import io
+from PIL import Image
+import pytesseract
+from wand.image import Image as wi
+
+pdf = wi(filename = "python.pdf", resolution = 300)
+pdfImage = pdf.convert('jpeg')
+
+imageBlobs = []
+
+for img in pdfImage.sequence:
+	imgPage = wi(image = img)
+	imageBlobs.append(imgPage.make_blob('jpeg'))
+
+recognized_text = []
+
+for imgBlob in imageBlobs:
+	im = Image.open(io.BytesIO(imgBlob))
+	text = pytesseract.image_to_string(im, lang = 'eng')
+	recognized_text.append(text)
+
+print(recognized_text)
